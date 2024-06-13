6de2be11620f584e010bd1e91a1b8051adff7a4f
조현승 <danitech171791@gmail.com>
2024-06-13 16:46:33 +0900

    [feat] GameLogicManager
    - 게임의 전반적인 로직을 담당함 (전역접 접근이 가능한 존재의 역할로써만 활용)
    - Unity 관련 코드가 섞이지 않아야함

---
+++ Assets/Scripts/Manager/GameLogicManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+
+public class Player
+{
+    public Player(int userId, string name)
+    {
+        UserId = userId;
+        Name = name;
+        Level = 0;
+    }
+
+    public int UserId { get; private set; }
+    public string Name { get; private set; }
+    public int Level { get; set; }
+}
+
+public class GameLogicManager
+{
+    private static GameLogicManager _instance = null;
+    private int _curSelectedPlayerId = 0;
+
+    private static Dictionary<int, Player> _playerDic = new Dictionary<int, Player>();
+    private Action<int, int> _levelUpCallback;
+
+    public static GameLogicManager Inst
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = new GameLogicManager();
+                TempInitPlayerList();
+            }
+            return _instance;
+        }
+    }
+
+    public static void TempInitPlayerList()
+    {
+        _playerDic.Add(1, new Player(1, "죠스바"));
+        _playerDic.Add(2, new Player(2, "쌍쌍바"));
+        _playerDic.Add(3, new Player(3, "바밤바"));
+    }
+
+    public void RegisterLevelUpCallback(Action<int, int> levelupCallback)
+    {
+        _levelUpCallback += levelupCallback;
+    }
+
+    public void UnRegisterLevelUpCallback(Action<int, int> levelupCallback)
+    {
+        _levelUpCallback -= levelupCallback;
+    }
+
+    public void RequestLevelUp()
+    {
+        int reqUserId = _curSelectedPlayerId;
+
+        if (_playerDic.ContainsKey(reqUserId))
+        {
+            var curPlayer = _playerDic[reqUserId];
+            curPlayer.Level++;
+            _levelUpCallback.Invoke(reqUserId, curPlayer.Level);
+        }
+    }
+
+    public void RefreshCharacterInfo(int requestId, Action<int, string, int> callback)
+    {
+        _curSelectedPlayerId = requestId;
+        if (_playerDic.ContainsKey(requestId))
+        {
+            var curPlayer = _playerDic[requestId];
+            callback.Invoke(curPlayer.UserId, curPlayer.Name, curPlayer.Level);
+        }
+    }
+}
