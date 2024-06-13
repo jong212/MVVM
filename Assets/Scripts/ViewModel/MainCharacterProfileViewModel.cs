99bd95016f8fec88c3b0f3b13b69ac80287cf5d2
조현승 <danitech171791@gmail.com>
2024-06-13 16:48:57 +0900

    [feat] MainProfile 뷰, 뷰모델, 뷰모델 Extension 추가

---
+++ Assets/Scripts/ViewModel/MainCharacterProfileViewModel.cs
@@ -0,0 +1,70 @@
+using System.ComponentModel;
+
+public class MainCharacterProfileViewModel
+{
+    private string _iconName;
+    private int _userId;
+    private string _name;
+    private int _level;
+
+    public string IconName
+    {
+        get { return _iconName; }
+        set
+        {
+            if (_iconName == value)
+                return;
+
+
+           _iconName = value;
+           OnPropertyChanged(nameof(IconName));
+        }
+    }
+
+    public int UserId
+    {
+        get { return _userId; }
+        set
+        {
+            if (_userId == value)
+                return;
+
+            _userId = value;
+            OnPropertyChanged(nameof(UserId));
+        }
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            if (_name == value)
+                return;
+
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+
+    public int Level
+    {
+        get { return _level; }
+        set
+        {
+            if (_level == value)
+                return;
+
+            _level = value;
+            OnPropertyChanged(nameof(Level));
+        }
+    }
+
+#region PropChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+#endregion
+}
