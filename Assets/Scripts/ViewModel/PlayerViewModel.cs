230420230015c59d782d02eda0cb95d8b781b628
조현승 <danitech171791@gmail.com>
2024-06-13 17:42:46 +0900

    [feat] Player 뷰, 뷰모델, 뷰모델 Extension 추가

---
+++ Assets/Scripts/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel;
+
+public class PlayerViewModel
+{
+    private int _userId;
+    private string _name;
+    private int _level;
+
+    public int UserId
+    {
+        get { return _userId; }
+        set { _userId = value; }
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set
+        {
+            if (_name == value)
+                return;
+
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+
+    public int Level
+    {
+        get { return _level; }
+        set
+        {
+            if (_level == value)
+                return;
+
+            _level = value;
+            OnPropertyChanged(nameof(Level));
+        }
+    }
+
+    #region PropChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+}
