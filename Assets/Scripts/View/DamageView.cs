c9473e133d24f35568bd3a090e19838f0297e823
조현승 <danitech171791@gmail.com>
2024-06-14 14:27:24 +0900

    [feat] MVVM - 캐릭터한테 데미지를 주는 UI 추가
    - 버튼이 눌러지면 캐릭터의 체력을 감소 시킬 것임

---
+++ Assets/Scripts/View/DamageView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageView : MonoBehaviour
+{
+    public void OnClick_GiveDamageToCharacter()
+    {
+    }
+}
