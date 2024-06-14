d8037d7d3013bbf818682da58cb259cb5c786051
조현승 <danitech171791@gmail.com>
2024-06-14 15:41:35 +0900

    [feat] Extension - namespace ViewModel.Extension으로 걸어주자
    - ViewModel따로, ViewModel.Extension 따로 범위를 나눠주기 위함

--- Assets/Scripts/Extensions/CharacterStateViewModelExtension.cs
+++ Assets/Scripts/Extensions/CharacterStateViewModelExtension.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CharacterStateViewModelExtension
+namespace ViewModel.Extension
 {
+    public class CharacterStateViewModelExtension
+    {
 
+    }
 }
+
