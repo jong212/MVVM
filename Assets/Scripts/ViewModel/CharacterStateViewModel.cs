90f2f2e2022342f5372a3ffe019fa604fdc6da81
조현승 <danitech171791@gmail.com>
2024-06-14 15:33:28 +0900

    [feat] ViewModel - 프로퍼티 추가
    - getter, setter를 이용해서 CharacterHP 추가
    - value가 같은 경우 갱신 이벤트 발생시키지 않도록 return 처리

--- Assets/Scripts/ViewModel/CharacterStateViewModel.cs
+++ Assets/Scripts/ViewModel/CharacterStateViewModel.cs
@@ -5,6 +5,19 @@ namespace ViewModel
 {
     public class CharacterStateViewModel
     {
+        private int _characterHp;
+
+        public int CharacterHp
+        {
+            get { return _characterHp; }
+            set
+            {
+                if (_characterHp == value)
+                    return;
+
+                _characterHp = value;
+            }
+        }
 
     }
 }
