7802a79f29f8ae031f090bd7d1b76a4295a5fcf6
조현승 <danitech171791@gmail.com>
2024-06-14 16:05:24 +0900

    [feat] View - 뷰에서 ViewModel을 바인딩 해봅시다

--- Assets/Scripts/View/CharacterStateView.cs
+++ Assets/Scripts/View/CharacterStateView.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
+using ViewModel;
+using ViewModel.Extension;
 
 public class CharacterStateView : MonoBehaviour
 {
     [SerializeField] Slider Slider_Hp;
+
+    private CharacterStateViewModel _vm;
+
 }
