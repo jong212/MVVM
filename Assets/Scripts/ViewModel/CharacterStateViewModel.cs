a8625a307ccac2dbe1ea63d4b61af96aa8f978d9
조현승 <danitech171791@gmail.com>
2024-06-14 15:31:06 +0900

    [feat] ViewModel - Mono제거, namespace ViewModel 추가
    - 뷰모델을 사용하는 경우에만 접근이 가능하도록 namespace ViewModel을 선언
    - 일반적으로 뷰모델은 뷰에 관한 처리를 직접하지 않으므로, using UnityEngine 제거

--- Assets/Scripts/ViewModel/CharacterStateViewModel.cs
+++ Assets/Scripts/ViewModel/CharacterStateViewModel.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class CharacterStateViewModel : MonoBehaviour
+namespace ViewModel
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CharacterStateViewModel
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
+
