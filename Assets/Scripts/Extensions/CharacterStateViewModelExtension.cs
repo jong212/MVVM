99de98d7fff93281556fc04512c6f27f7edf0c04
조현승 <danitech171791@gmail.com>
2024-06-14 15:47:15 +0900

    [feat] Extension - 필요 함수들 추가
    - static 함수여야하며, static 함수이기 때문에 대부분의 처리가 파라미터(매개변수)와 return(반환형)으로만 이루어져야함 (static 변수들이 사용되지 않게 주의)
    - 첫번째 파라미터로 this를 넣어주는 이유는 Extension을 사용하기 위한 규칙중의 하나, 런타임시 해당 함수를 호출하는 객체를 의미

--- Assets/Scripts/Extensions/CharacterStateViewModelExtension.cs
+++ Assets/Scripts/Extensions/CharacterStateViewModelExtension.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 
 namespace ViewModel.Extension
 {
-    public class CharacterStateViewModelExtension
+    public static class CharacterStateViewModelExtension
     {
+        public static void RefreshViewModel(this CharacterStateViewModel vm)
+        {
+        }
 
+        public static void OnRefreshViewModel(this CharacterStateViewModel vm)
+        {
+        }
+
+        public static void RegisterHpChangedEvent(this CharacterStateViewModel vm, bool isRegister)
+        {
+        }
+
+        public static void OnResponseHpChangedEvent(this CharacterStateViewModel vm, int userId, int hp)
+        {
+        }
     }
 }
 
